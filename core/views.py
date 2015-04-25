44cd00fca4ebb55b4137ed71f0df366d59cb11d3
mrraghur <iamanonymouscoward@gmail.com>
2015-04-25 08:10:41 -0700

    inconsequenial commit 1

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
+#Create a hello world view
 # Create your views here.
 def TestView(request, **kwargs):
     return HttpResponse("Hello world");
